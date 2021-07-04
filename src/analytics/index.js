a8e1a16b5e338944b366a0a641f2e80577f2a185
ankit.bansal@gupshup.io <ankit.bansal@gupshup.io>
2021-07-05 00:22:45 +0530

    adding analytics property

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('UA-xxxxxxxxx-1');
+  ReactGA.initialize('UA-92632261-2');
 };
 
 export const logPageView = () => {
